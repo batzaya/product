373be7fb155ba60507a5783f039e77f379033c5c
Batzaya <hhzaya@gmail.com>
2013-05-29 13:37:27 +0800

    бараа захилах хуудасны алдаануудыг зассан.

--- template/buy_product.php
+++ template/buy_product.php
@@ -2,16 +2,20 @@
 <form action="" method="POST" style="margin-left:300px">
     Таны нэр: <br/><input type="text" name="name"/><br/>
     Таны хаяг:<br/> <textarea name="address" rows="10" cols="30"></textarea><br/>
-    <input type="submit" value="илгээх" style="margin-left:200px">
+    <input type="submit" value="илгээх" style="margin-left:200px"><br>
 </form>
-<?php echo $msg . "<br>";?>
+
+<?php 
+if (isset($msg))
+    echo $msg;
+?>
 
 <div style="margin-left:800px; margin-top:-250px">
     <h3>Таны сонгосон бараа</h3><br/>
     <table>
-        <tr><td>Нэр: <?php echo $product['product_name'];?></td></tr>
-        <tr><td>Төрөл: <?php echo $product['type'];?></td></tr>
-        <tr><td>Тайлбар: <?php echo $product['content'];?></td></tr>
+        <tr><td>Нэр: <?php echo $product['product_name']; ?></td></tr>
+        <tr><td>Төрөл: <?php echo $product['type']; ?></td></tr>
+        <tr><td>Тайлбар: <?php echo $product['content']; ?></td></tr>
         <tr><td>Үнэ: <?php echo $product['price']; ?></td></tr>
     </table>
 </div>
